7c5cc2e8adb093bfc2443af1306bb64d8f8ff4ee
mmesutkayaa <85591207+mmesutkayaa@users.noreply.github.com>
2023-01-25 03:55:21 +0300

    Add files via upload

---
+++ SimpsonBook/SimpsonBook/Simpson.swift
@@ -0,0 +1,22 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Mesut Kaya on 9.11.2022.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    
+    var name: String
+    var job: String
+    var image: UIImage
+    
+    init(name: String, job: String, image: UIImage) {
+        self.name = name
+        self.job = job
+        self.image = image
+    }
+}
